40eda14d5e49a73a09268595d2afb17f5098c7bb
mike-kun-xu <mike.kun.xu@gmail.com>
2020-12-01 14:04:22 -0500

    Make some simplification

--- sort.ex
+++ sort.ex
@@ -1,17 +1,14 @@
 defmodule Sort do
-    def quicksort(list) do
-    cond do
-        length(list) > 1 ->
+    def quicksort(list) when length(list) > 1 do
         pivot = hd(list)
         large_list = Enum.filter(list, fn x -> x > pivot end)
         small_list = Enum.filter(list, fn x -> x < pivot end)
         pivot_list = Enum.filter(list, fn x -> x == pivot end)
         List.flatten([Sort.quicksort(large_list), pivot_list, Sort.quicksort(small_list)])
-        length(list) == 1 -> list
-        Enum.empty?(list) -> []
     end
-    end
-
+    def quicksort(list) when length(list) == 1 do list end    
+    def quicksort(list) when length(list) == 0 do [] end
+    
     def mergesort(list) when length(list) > 1 do
         l = Enum.take_every(list, 2)
         r = Enum.drop_every(list, 2)
@@ -20,12 +17,11 @@ defmodule Sort do
     def mergesort(list) when length(list) == 1 do list end
     def mergesort(list) when length(list) == 0 do [] end
 
-    def merge(l, r, c) when length(l) >= 1 and length(r) >= 1 do
-        if hd(l) > hd(r) do 
-            Sort.merge(tl(l), r, c ++ [hd(l)])
-        else
-            Sort.merge(l, tl(r), c ++ [hd(r)])
-        end
+    def merge(l = [lh|lt], r = [rh|rt], c) when lh > rh do
+        Sort.merge(lt, r, c ++ [lh])
+    end
+    def merge(l = [lh|lt], r = [rh|rt], c) do
+        Sort.merge(l, rt, c ++ [rh])
     end
     def merge(l, [], c) do
         c ++ l
